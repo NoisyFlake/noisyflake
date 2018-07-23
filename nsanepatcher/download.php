9cfdbd4961b23a445a43d73f93adec80e853ee55
NoisyFlake <contact@noisyflake.com>
2018-07-23 21:01:07 +0200

    Add NSanePatcher 1.0.2 and screenshots

--- nsanepatcher/download.php
+++ nsanepatcher/download.php
@@ -6,7 +6,7 @@ fwrite($f, $current_count + 1);
 fclose($f);
 
 // header("Location: dl/nsanepatcher-v1.0.0.zip");
-$filename = "nsanepatcher-v1.0.1.zip";
+$filename = "nsanepatcher-v1.0.2.zip";
 header("Content-disposition: attachment; filename=".$filename."");
 header('Content-type: application/zip');
 readfile("dl/" . $filename);
