ff63ceeda25aec6e31ef0887ad56893aae0100eb
Joel Poick <joel.poick@cycos.com>
2019-07-18 09:28:05 +0200

    Replace index with under construction page

--- main/index.php
+++ main/index.php
@@ -1,184 +1,42 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>NoisyFlake</title>
 
-  <head>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.css">
+	<style type="text/css">
+		p {
+			color: #999;
+			padding-top: 3rem;
+			font-family: monospace;
+			white-space: pre;
+		}
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Web and iOS development">
-    <meta name="author" content="NoisyFlake">
+		.reason {
+			color: #000;
+		}
+	</style>
+</head>
+<body>
+	<div class="ui container">
 
-    <title>NoisyFlake</title>
+<p>[   20.752000] Kernel unaligned instruction access[#1]:
+[   20.752000] Cpu 0
+[   20.752000] Stack : 86b77600 ffffffed 86921860 86921800 66720200 86b70ee7 86b88180 fffffff4
+[   20.752000]         86921860 d0a89d00 00000000 86b4b080 d0a8f1ac 86b77600 d0a8efc0 8056cbc8
+[   20.752000]         86b77600 d0a8ef90 d0a8efc0 d0a8efc0 80779210 0000001c d0a7dae4 8056d2c0
+[   20.752000]         00000000 00000001 80779210 86b4b080 00000000 00000000 86b77620 80533fbc
+[   20.752000]         0000001c 804b9bd8 d0a7dae4 0000001b 86b77620 86b776c8 d0a8efc0 805340d0
+[   20.752000]         ..
+[   20.752000]
+[   20.752000]
+[   20.752000] Code: (Bad address in epc)
+[   20.752000]
+[   21.000000] Kernel panic - Not syncing: <span class="reason">Under Construction. Please come back later.</span></p>
 
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-
-    <link href="css/noisyflake.min.css" rel="stylesheet">
-
-  </head>
-
-  <body id="page-top">
-
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">NoisyFlake</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#about">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#tweaks">Tweaks</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <header class="masthead" id="about">
-      <div class="header-content">
-        <div class="header-content-inner">
-          <h1 id="homeHeading">NOISYFLAKE</h1>
-          <hr>
-          <p>23y/o software developer</p>
-          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#tweaks">View my iOS Tweaks</a>
-        </div>
-      </div>
-    </header>
-
-    <section id="tweaks">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 text-center">
-            <h2 class="section-heading">Published Tweaks</h2>
-            <hr class="primary">
-          </div>
-        </div>
-      </div><br>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4 col-md-8 text-center">
-            <div class="service-box">
-              <i class="fa fa-4x fa-fire text-primary sr-icons"></i>
-              <h3>Magma</h3>
-              <p class="text-muted">Color your Control Center</p><br>
-
-              <p>iOS 11 - 12</p>
-              <small class="text-muted"><a href="cydia://url/https://cydia.saurik.com/api/share#?source=https://apt.noisyflake.com/&package=com.noisyflake.magma">My Repo</a>, Free</small><br>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-8 text-center">
-            <div class="service-box">
-              <i class="fa fa-4x fa-tags text-primary sr-icons"></i>
-              <h3>Goodges 2</h3>
-              <p class="text-muted">Replace the notification badges with colored text labels. Successor to Goodges</p><br>
-
-              <p>iOS 10 - 12</p>
-              <small class="text-muted"><a href="cydia://url/https://cydia.saurik.com/api/share#?source=https://apt.noisyflake.com/&package=com.noisyflake.goodges2">My Repo</a>, Free</small><br>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-8 text-center">
-            <div class="service-box">
-              <i class="fa fa-4x fa-wifi text-primary sr-icons"></i>
-              <h3>WiFiCarrier</h3>
-              <p class="text-muted">Replace the carrier in the status bar with the current WiFi SSID</p><br>
-
-              <p>iOS 12</p>
-              <small class="text-muted"><a href="cydia://url/https://cydia.saurik.com/api/share#?source=https://apt.noisyflake.com/&package=com.noisyflake.wificarrier">My Repo</a>, Free</small><br>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-8 text-center" style="margin-bottom: 50px">
-            <div class="service-box">
-              <i class="fa fa-4x fa-minus text-primary sr-icons"></i>
-              <h3>ModernDock</h3>
-              <p class="text-muted">Enable the modern iPhone X dock for all older devices with a regular dock</p><br>
-
-              <p>iOS 11 - 12</p>
-              <small class="text-muted"><a href="cydia://url/https://cydia.saurik.com/api/share#?source=https://apt.noisyflake.com/&package=com.noisyflake.moderndock">My Repo</a>, Free</small><br>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-8 text-center">
-            <div class="service-box">
-              <i class="fa fa-4x fa-tablet text-primary sr-icons"></i>
-              <h3>iPadStatusBar</h3>
-              <p class="text-muted">Enable the modern iPad status bar for all devices, allows hiding status bar items</p><br>
-
-              <p>iOS 12</p>
-              <small class="text-muted"><a href="cydia://url/https://cydia.saurik.com/api/share#?source=https://apt.noisyflake.com/&package=com.noisyflake.ipadstatusbar">My Repo</a>, Free</small><br>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-8 text-center">
-            <div class="service-box">
-              <i class="fa fa-4x fa-share text-primary sr-icons"></i>
-              <h3>ShortcutEnabler</h3>
-              <p class="text-muted">Enable the modern iOS flashlight / camera shortcuts on the lockscreen for all devices</p><br>
-
-              <p>iOS 11 - 12</p>
-              <small class="text-muted"><a href="cydia://url/https://cydia.saurik.com/api/share#?source=https://apt.noisyflake.com/&package=com.noisyflake.shortcutenabler">My Repo</a>, Free</small><br>
-            </div>
-          </div>
-
-          <div class="col-lg-12 col-md-24 text-center">
-            <div class="service-box">
-              <i class="fa fa-4x fa-power-off text-primary sr-icons"></i>
-              <h3>NoLSPowerDown</h3>
-              <p class="text-muted">Prevent the shutdown screen from appearing on the lockscreen</p><br>
-
-              <p>iOS 7 - 12</p>
-              <small class="text-muted"><a href="cydia://url/https://cydia.saurik.com/api/share#?source=https://apt.noisyflake.com/&package=com.noisyflake.nolspowerdown">My Repo</a>, Free</small><br>
-            </div>
-          </div>
-
-      </div>
-
-      </div>
-    </section>
-
-    <section id="contact">
-    <div class="call-to-action bg-dark">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto text-center">
-            <h2 class="section-heading">Contact me</h2>
-            <hr class="primary">
-            <p>If you have a problem with one of my tweaks or have a suggestion, please let me know it.</p>
-          </div>
-        </div><br>
-        <div class="row">
-          <div class="col-lg-12 mr-auto text-center">
-            <i class="fa fa-envelope-o fa-3x sr-contact"></i><br><br>
-            <p>
-              <a href="https://www.reddit.com/message/compose/?to=NoisyFlake">/u/NoisyFlake</a>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-    </section>
-
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/popper/popper.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
-
-    <script src="js/creative.min.js"></script>
-
-  </body>
-
+	</div>
+</body>
 </html>
